2f32967a168173fc939a85aeff7889248a8a5192
Kaneesha Dawood <kaneesha.dawood@gmail.com>
2020-06-07 11:01:32 -0400

    Adding files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Running an experiment to see if there is an increase in the number of fans for metal bands based on its country.',
+    author='Kaneesha Dawood',
+    license='',
+)
